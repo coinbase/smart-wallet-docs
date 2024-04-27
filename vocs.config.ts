7c1170d018c4c3a1c51f94bc3a526ed887b78a40
Wilson Cusack <wilson.cusack@coinbase.com>
2024-04-27 14:42:46 -0400

    styles + format

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'Docs',
+  theme: {
+    variables: {
+      color: {
+        textAccent: { light: '#2394ff', dark: '#e9e9ea' },
+        backgroundDark: {light: '#ebeaee', dark: 'black'},
+        background: {light: '#f6f5f8', dark: 'black'}, //f6f5f8 /ebf1f8
+        heading: { light: 'black', dark: '#e9e9ea' },
+        text3: { light: 'black', dark: '#e9e9ea' },
+        background5: { light: '#dee8ff', dark: '#3c393f' },
+      }
+    }
+  },
+  title: 'Smart Wallet',
+  topNav: [ 
+    { text: 'Guide & API', link: '/docs/getting-started', match: '/docs' }, 
+    { text: 'Blog', link: '/blog' }, 
+    { 
+      text: 'version', 
+      items: [ 
+        { 
+          text: 'Changelog', 
+          link: 'https://github.com/wevm/vocs/blob/main/src/CHANGELOG.md', 
+        }, 
+        { 
+          text: 'Contributing', 
+          link: 'https://github.com/wevm/vocs/blob/main/.github/CONTRIBUTING.md', 
+        }, 
+      ], 
+    }, 
+  ], 
   sidebar: [ 
     { 
       text: 'Why Smart Wallet?', 
